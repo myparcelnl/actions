ccce3121383815ee7afd4a0f7c304439cf32ddac
Edie Lemoine <edie@myparcel.nl>
2022-05-18 12:54:10 +0200

    build: update publish script

--- publish.js
+++ publish.js
@@ -3,7 +3,7 @@ const { spawnSync } = require('child_process');
 
 const [, , version] = process.argv;
 
-(async() => {
+(() => {
   if (prerelease(version)) {
     console.info('Prerelease version detected; will not add a major version tag.');
     return;
@@ -13,10 +13,10 @@ const [, , version] = process.argv;
 
   for (const v of versionTags) {
     console.info(`Deleting tag v${v} from origin.`);
-    await spawnSync(`git push origin :refs/tags/v${v}`, { stdio: 'inherit' });
+    spawnSync('git', ['push', 'origin', `:refs/tags/v${v}`], { stdio: 'inherit' });
 
     console.info(`Pushing new tag v${v} to git.`);
-    await spawnSync(`git tag --force v${v}`, { stdio: 'inherit' });
-    await spawnSync(`git push origin v${v}`, { stdio: 'inherit' });
+    spawnSync('git', ['tag', '--force', `v${v}`], { stdio: 'inherit' });
+    spawnSync('git', ['push', 'origin', `v${v}`], { stdio: 'inherit' });
   }
 })();
