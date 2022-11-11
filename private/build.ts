1e37fe19836722725466eaac4859c9de39691636
Edie Lemoine <edie@myparcel.nl>
2022-11-11 15:21:15 +0100

    build: use yarn 2 zero installs

--- private/build.ts
+++ private/build.ts
@@ -1,38 +1,45 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers,no-console */
+import {build} from 'tsup';
 import fs from 'fs';
 import path from 'path';
-import {build} from 'tsup';
 
 const IGNORE_DIRS = ['node_modules'];
+const PNP_LOADER = path.resolve(__dirname, '..', '.pnp.cjs');
 
-(async () => {
+void (async (): Promise<void> => {
   const directory = process.argv[2] ?? process.cwd();
 
   const promises = fs
     .readdirSync(directory)
     // Check if file/dir is a directory
-    .filter(file => fs.statSync(path.resolve(directory, file)).isDirectory())
+    .filter((file) => fs.statSync(path.resolve(directory, file)).isDirectory())
     // Check if dir does not start with . and is not present in IGNORE_DIRS
-    .filter(dir => !dir.startsWith('.') || IGNORE_DIRS.includes(dir))
+    .filter((dir) => !dir.startsWith('.') || IGNORE_DIRS.includes(dir))
     // Check if action has an index.ts file
-    .filter(action => fs.existsSync(path.resolve(directory, action, 'index.ts')))
-    .map(async action => {
+    .filter((action) => fs.existsSync(path.resolve(directory, action, 'index.ts')))
+    .map(async (action) => {
       const actionDir = path.resolve(directory, action);
-      let distDir = path.resolve(actionDir, 'dist');
+      const distDir = path.resolve(actionDir, 'dist');
 
       console.log(`Compiling JS action: ${action}`);
+      const pnpLoaderPath = path.relative(distDir, PNP_LOADER);
+
       await build({
         entry: [path.resolve(actionDir, 'index.ts')],
         outDir: distDir,
         target: 'node16',
-        minify: true
+        minify: true,
+        banner: {
+          js: `require('${pnpLoaderPath}').setup();`,
+        },
       });
     });
 
   if (!promises.length) {
+    // eslint-disable-next-line no-console
     console.log('No actions to compile found.');
     return;
   }
 
   await Promise.all(promises);
 })();
-
--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 const baseConfig = require('@myparcel/semantic-release-config');
-const { addGitPlugin, addExecPlugin, addGitHubPlugin } = require('@myparcel/semantic-release-config/src/plugins');
-const { gitPluginDefaults } = require('@myparcel/semantic-release-config/src/plugins/addGitPlugin');
+const {addGitPlugin, addExecPlugin, addGitHubPlugin} = require('@myparcel/semantic-release-config/src/plugins');
+const {gitPluginDefaults} = require('@myparcel/semantic-release-config/src/plugins/addGitPlugin');
 
 module.exports = {
   extends: '@myparcel/semantic-release-config',
@@ -8,10 +8,10 @@ module.exports = {
   plugins: [
     ...baseConfig.plugins,
     addExecPlugin({
-      prepare: 'yarn compile && yarn build',
+      prepareCmd: 'yarn compile && yarn build',
     }),
     addGitPlugin({
-      assets: [...gitPluginDefaults.assets, './**/dist/*.js'],
+      assets: [...gitPluginDefaults.assets, './**/dist/**/*.js'],
     }),
     addGitHubPlugin(),
   ],
