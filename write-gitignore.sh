2fc4688ae09a88e1a4fac5b716f06ec1500aee64
Edie Lemoine <edie@myparcel.nl>
2024-08-01 14:53:43 +0200

    fix(deps): check proper node_modules in to git

---
+++ write-gitignore.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+# Add production dependencies and all their dependencies to .gitignore. These dependencies need to be checked in for js
+# actions to work.
+
+deps=$(npm pkg get dependencies --json)
+
+allDependencies="[]"
+fetchedDepsFor=()
+
+function get-deps-recursive() {
+  package=$1
+
+  dependencies=$(yarn npm info $package --json | jq '.dependencies')
+  fetchedDepsFor+=($package)
+
+  if [ "$dependencies" == "null" ]; then
+    return
+  fi
+
+  dependencyKeys=$(echo $dependencies | jq -r 'keys[]')
+
+  # if package is scoped, add the namespace to the list of dependencies
+  packageNamespace=$(echo $package | cut -d'/' -f1)
+  allDependencies=$(echo $allDependencies | jq ". + [\"$packageNamespace\"]")
+  allDependencies=$(echo $allDependencies | jq ". + [\"$package\"]")
+
+  for dep in $dependencyKeys; do
+    allDependencies=$(echo $allDependencies | jq ". + [\"$dep\"]")
+  done
+
+  for dep in $dependencyKeys; do
+    if [[ " ${fetchedDepsFor[@]} " =~ " ${dep} " ]]; then
+      echo "Skipping $dep as it has already been fetched."
+      continue
+    fi
+
+    echo "Fetching deps for $dep"
+    get-deps-recursive $dep
+  done
+}
+
+for dep in $(echo $deps | jq -r 'keys[]'); do
+  get-deps-recursive $dep
+done
+
+# remove duplicates
+allDependencies=$(echo $allDependencies | jq 'unique | sort')
+
+startMarker="# <!-- start automatically generated entries -->"
+endMarker="# <!-- end automatically generated entries -->"
+
+# delete the old markers and content
+sed -i .bak "/$startMarker/,/$endMarker/d" ".gitignore"
+rm -f ".gitignore.bak"
+
+# add the new content
+echo "$startMarker" >> ".gitignore"
+echo "node_modules/*" >> ".gitignore"
+
+for dep in $(echo $allDependencies | jq -r '.[]'); do
+  echo "!node_modules/$dep/" >> ".gitignore"
+done
+
+echo "$endMarker" >> ".gitignore"
